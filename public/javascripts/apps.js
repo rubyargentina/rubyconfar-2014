834abb01be6b80d5d251ac4c18cab804e97fd425
Nico Sztabzyb <nico@szstudios.net>
2014-08-03 20:54:09 -0300

    Added Trinity with Matrix effect. Custom phrasing for each character. Added Jquery for responsive headlines. Not bad for a hangover sunday.

--- public/javascripts/apps.js
+++ public/javascripts/apps.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var arr = ['horseboy', 'erlich', 'woz', 'moss', 'staplerguy'];
+  var arr = ['horseboy', 'erlich', 'woz', 'moss', 'staplerguy', 'trinity'];
   var character = window.location.hash.substr(1);
 
   if ($.inArray(character, arr)!==-1) {
@@ -35,6 +35,108 @@ $(document).ready(function() {
   });
 
 
+  // Center trinity
+  centerTrinity = function(){
+    var Trinity =  $('img#matrix').width();
+    $('img#matrix').css({'marginLeft' : Trinity / -2});
+  }
+  centerTrinity();
+
+  // Responsive Headlines
+  $('section#start h1.trinity').fitText(
+    1.2, { 
+      minFontSize: '30px', 
+      maxFontSize: '70px' 
+  });
+
+  $('section#start h1.moss').fitText(
+    1.2, { 
+      minFontSize: '40px', 
+      maxFontSize: '90px' 
+  });
+
+  $('section#start h1.erlich').fitText(
+    1.2, { 
+      minFontSize: '47px', 
+      maxFontSize: '90px' 
+  });
+
+  $('section#start h1.horseboy').fitText(
+    1.2, { 
+      minFontSize: '36px', 
+      maxFontSize: '70px' 
+  });
+
+  $('section#start h1.staplerguy').fitText(
+    1.2, { 
+      minFontSize: '36px', 
+      maxFontSize: '70px' 
+  });
+
+
+
+
+  // Matrix
+  var c = document.getElementById("c");
+  var ctx = c.getContext("2d");
+
+  //making the canvas full screen
+  c.height = window.innerHeight;
+  c.width = window.innerWidth;
+
+  //chinese characters - taken from the unicode charset
+  var chinese = "rubyconfargentina2014szstudios";
+  //converting the string into an array of single characters
+  chinese = chinese.split("");
+
+  var font_size = 10;
+  var columns = c.width/font_size; //number of columns for the rain
+  //an array of drops - one per column
+  var drops = [];
+  //x below is the x coordinate
+  //1 = y co-ordinate of the drop(same for every drop initially)
+  for(var x = 0; x < columns; x++)
+    drops[x] = 1; 
+
+  //drawing the characters
+  function draw()
+  {
+    //Black BG for the canvas
+    //translucent BG to show trail
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillRect(0, 0, c.width, c.height);
+    
+    ctx.fillStyle = "#0F0"; //green text
+    ctx.font = font_size + "px arial";
+    //looping over drops
+    for(var i = 0; i < drops.length; i++)
+    {
+      //a random chinese character to print
+      var text = chinese[Math.floor(Math.random()*chinese.length)];
+      //x = i*font_size, y = value of drops[i]*font_size
+      ctx.fillText(text, i*font_size, drops[i]*font_size);
+      
+      //sending the drop back to the top randomly after it has crossed the screen
+      //adding a randomness to the reset to make the drops scattered on the Y axis
+      if(drops[i]*font_size > c.height && Math.random() > 0.975)
+        drops[i] = 0;
+      
+      //incrementing Y coordinate
+      drops[i]++;
+    }
+  }
+
+  setInterval(draw, 33);
+
+
+
+
+
+
+
+
+
+
 });
 
 $(window).resize(function() {
@@ -45,6 +147,8 @@ $(window).resize(function() {
   if (windowWidth > 500) {
     $('nav').attr('style', ''); // removes display: none
   }
+
+  centerTrinity();
 
 });
 
@@ -64,8 +168,5 @@ $(window).scroll(function(){
 });
 
   // Cleans URL of dirty hashtag.
-  $(window).load(function(){
-    history.pushState("", document.title, window.location.pathname);
-  });
-
+  
 
